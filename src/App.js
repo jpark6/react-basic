f989725388d9e9e9eff236a88b6fdda8b2bf3da9
Jake Park <qkstjr09@gmail.com>
2021-06-30 19:26:32 +0900

    React Hook useState, useEffect 적용

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import {useState, useEffect} from "react";
 import './App.css'
 import Subject from "./Components/Subject"
 import TOC from "./Components/TOC"
@@ -9,11 +10,32 @@ import Content from "./Components/Content"
  * @constructor
  */
 export default function App() {
+  const [data, setData] = useState({
+    subject: { title: 'Web', sub: '🙋‍♂️ Header!!!', },
+    toc: [
+      { id: 1, title: 'HTML', desc: 'HTML is for Information' },
+      { id: 2, title: 'CSS', desc: 'CSS is for Style' },
+      { id: 3, title: 'JavaScript', desc: 'JavaScript is for Interactive' },
+    ],
+  })
+  
+  const addTOC = (new_toc) => {
+    const tocArr = data.toc
+    new_toc.id = tocArr.length + 1
+    tocArr.push(new_toc)
+    setData({...data, contents:tocArr})
+  }
+  
+  useEffect(()=> {
+    addTOC({ title: 'Markdown', desc: 'Markdown is for Document' })
+    console.log(data)
+  }, [])
+  
   return (
     <div className="App">
-      <Subject title="Web" sub="Header!!"/>
+      <Subject title={data.subject.title} sub={data.subject.sub} />
       <Subject title="React" sub="Awesome"/>
-      <TOC />
+      <TOC contents={data.toc}/>
       <Content title="HTML" desc="HTML is HyperText Markup Language."/>
       <Content title="CSS" desc="CSS is Cascading Style Sheet"/>
       <Content title="JavaScript" desc="JavaScript is Awesome"/>
--- src/Components/TOC.js
+++ src/Components/TOC.js
@@ -1,17 +1,18 @@
 /**
  * TOC : Table Of Content 목차
+ * @param {Object} props - 콘텐츠 배열
+ * @param {[{id:number, title:string, desc:string}]} props.contents - 콘텐츠 배열
  * @returns {JSX.Element}
  * @constructor
  */
-export default function TOC() {
-  const contents = ['HTML', 'CSS', 'JavaScript']
+export default function TOC(props) {
   return (
     <nav>
       <ul>
         {
-          contents.map((content) => (
-            <li key={content}>
-              <a href={"#" +content.toLowerCase()}>{content}</a>
+          props.contents.map((content) => (
+            <li key={content.id}>
+              <a href={"#" +content.title.toLowerCase()}>{content.title}</a>
             </li>
           ))
         }
